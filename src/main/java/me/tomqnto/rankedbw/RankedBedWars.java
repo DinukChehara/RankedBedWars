460486d9889b3467411b5353095ccbdd19e6bf75
Tomqnto <dinukchehara24@gmail.com>
2026-08-22 10:29:34 +0530

    initial commit

---
+++ src/main/java/me/tomqnto/rankedbw/RankedBedWars.java
@@ -0,0 +1,15 @@
+package me.tomqnto.rankedbw;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RankedBedWars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
